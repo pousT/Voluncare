182d91eb59132cb926973ceaef67d7274f3e329e
zy16373 <zy16373@gmail.com>
2016-11-23 18:08:24 +0800

    add schema for activity

--- db/models.js
+++ db/models.js
@@ -8,6 +8,18 @@ module.exports = {
         gender:{type:String,required:true},
         credit:{type:Number,default:0},
         status:{type:Number,default:0}
+    },
+
+    activity:{
+        title:{type:String, required:true},
+        description:{type:String, required:true},
+        start:{type:Date, required:true},
+        end:{type:Date},
+        address:{type:String, required:true},
+        creditReq:{type:Number,default:0},
+        statusReq:{type:Number,default:0},
+        credit:{type:Number,required:true},
+        maxNumber:{type:Number, required:true}
     }
 
 
