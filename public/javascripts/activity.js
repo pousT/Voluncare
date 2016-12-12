28fde86f40f8b4f7f25f9dbc4f0445c1469de07a
zy16373 <zy16373@gmail.com>
2016-12-12 12:49:21 +0800

    save change before switch branch

--- public/javascripts/activity.js
+++ public/javascripts/activity.js
@@ -1,14 +1,15 @@
 var app = angular.module('myApp', ['ionic','ngMessages', 'ui.router']);
     app.controller('actCtrl', function ($state, $scope, $http) {
+        $scope.signed = false;
         $scope.participate = function() {
          $http({
             url:'/participate',
             method: 'GET',
             data: {}  
         }).success(function(data,status){
-            if(status == 200) {
-                
-            }
+
+                $scope.signed = true;
+
 
         }).error(function(status, data){
             console.log(data);
