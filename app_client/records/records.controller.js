99dde496ac3475e32532b4dd654f372018d178b3
zy16373 <zy16373@gmail.com>
2017-02-21 18:59:16 +0800

    add find user by telephone functions

--- app_client/records/records.controller.js
+++ app_client/records/records.controller.js
@@ -13,12 +13,14 @@ function recordsCtrl(recordsData,$scope,$state) {
         message = "Sorry, something's gone wrong ";
     });
 
-    $scope.findUserByTelephone(telephone) {
+    $scope.findUserByTelephone = function(telephone) {
         recordsData.findUserByTelephone(telephone).success(function(data) {
             //成功则data为查询到的用户，其中包含用户_id, telephone, name三个字段，如果电话不存在，则data为空
             if(data) {
-                $scope.foundUser = data;
-                
+                $scope.foundUser = data; // 如果用户存在，则储存在foundUser变量中
+
+            } else {
+                //这里处理用户不存在
             }
         }).error(function (e) {
             console.log(e);
