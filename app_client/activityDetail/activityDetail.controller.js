103e40b862390450bb18e22376388da38a9f9c9a
Lao235 <ggg_007@qq.com>
2017-02-25 14:29:22 +0800

    fix alert
    now can print 'sign up successfully'

--- app_client/activityDetail/activityDetail.controller.js
+++ app_client/activityDetail/activityDetail.controller.js
@@ -1,6 +1,6 @@
 angular
 .module('myApp')
-.controller('activityDetailCtrl',['$scope','$stateParams','activityData',function($scope, $stateParams,activityData,$ionicPopup, $timeout){
+.controller('activityDetailCtrl',['$scope','$stateParams','activityData', '$ionicPopup', '$timeout', function($scope, $stateParams,activityData,$ionicPopup, $timeout){
     $scope.activity = $stateParams.activity;
     activityData.getActivityById($scope.activity._id).success(function (data) {
         message = data.length > 0 ? "" : "暂无数据";
@@ -21,4 +21,5 @@ angular
              });
         });
     }
+
 }])
