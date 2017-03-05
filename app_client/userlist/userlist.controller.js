cab1096cd743c05db9e74e5ecbd1bf966da6d2e0
zy16373 <zy16373@gmail.com>
2017-03-05 13:33:08 +0800

    用户列表读取

--- app_client/userlist/userlist.controller.js
+++ app_client/userlist/userlist.controller.js
@@ -3,8 +3,14 @@ angular
 .controller('userlistCtrl', userlistCtrl);
 
 
-userlistCtrl.$inject = ['$scope','$state'];
-function userlistCtrl($scope,$state) {
-   
+userlistCtrl.$inject = ['$scope','$state','userData'];
+function userlistCtrl($scope,$state, userData) {
+    userData.users().success(function (data) {
+        message = data.length > 0 ? "" : "暂无数据";
+        $scope.users = data;
+    }).error(function (e) {
+        console.log(e);
+        message = "Sorry, something's gone wrong ";
+    });   
 
 }
