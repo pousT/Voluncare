51ae6082466711152169dfbe6d94f9980fc12dee
Shangrongcai <zy15756@nottingham.edu.cn>
2017-03-16 09:43:24 +0800

    showDiscount

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -6,4 +6,17 @@ angular
 homeCtrl.$inject = ['authentication','$scope'];
 function homeCtrl(authentication,$scope) {
     $scope.user = authentication.currentUser();
+
+    $scope.showDiscount = function() {
+    	if($scope.user.credit >= 2000){
+    		window.alert("9.2折");
+    	} else if($scope.user.credit >= 500){
+    		window.alert("9.5折");
+    	} else if($scope.user.credit >= 100){
+    		window.alert("9.8折");
+    	} else {
+    		window.alert("没有折扣");
+    	}
+    }
 }
+
